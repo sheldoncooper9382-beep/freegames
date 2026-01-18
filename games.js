70ad38b860c518f1fd528ec7f320b9f90496929d
Freetoolhub <sheldoncooper9382@gmail.com>
2026-01-18 15:30:18 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,34 +1,9 @@
 const games = [
-  {
-    title: "Tetris",
-    description: "Stack blocks and clear lines",
-    icon: "ph-cube",
-    path: "games/tetris/index.html"
-  },
-  {
-    title: "Snake",
-    description: "Eat, grow, survive",
-    icon: "ph-snake",
-    path: "games/snake/index.html"
-  },
-  {
-    title: "Pong",
-    description: "Classic paddle duel",
-    icon: "ph-tennis-ball",
-    path: "games/pong/index.html"
-  },
-  {
-    title: "Breakout",
-    description: "Smash every brick",
-    icon: "ph-bricks",
-    path: "games/breakout/index.html"
-  },
-  {
-    title: "Space Invaders",
-    description: "Defend the galaxy",
-    icon: "ph-alien",
-    path: "games/space-invaders/index.html"
-  }
+  { title: "Tetris", description: "Stack blocks and clear lines", icon: "🟦", path: "games/tetris/index.html" },
+  { title: "Snake", description: "Eat, grow, survive", icon: "🐍", path: "games/snake/index.html" },
+  { title: "Pong", description: "Classic paddle duel", icon: "🏓", path: "games/pong/index.html" },
+  { title: "Breakout", description: "Smash every brick", icon: "🧱", path: "games/breakout/index.html" },
+  { title: "Space Invaders", description: "Defend the galaxy", icon: "👾", path: "games/space-invaders/index.html" }
 ];
 
 const grid = document.getElementById("gamesGrid");
@@ -39,9 +14,7 @@ games.forEach(game => {
   card.onclick = () => location.href = game.path;
 
   card.innerHTML = `
-    <div class="game-icon">
-      <i class="ph ${game.icon}"></i>
-    </div>
+    <div class="game-icon">${game.icon}</div>
     <div class="game-title">${game.title}</div>
     <div class="game-desc">${game.description}</div>
   `;
